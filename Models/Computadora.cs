02b81423b9bce61471f3c75314a6c2f9d071894d
orio17a <orianafa17012006@gmail.com>
2026-08-19 20:33:43 -0300

    Add files via upload

---
+++ Models/Computadora.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi2026.Models
+{
+    public class Computadora
+    {
+        public string procesador { get; set; }
+        public int ram { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public List <string> programasInstalados { get; set; }
+
+    }
+}
